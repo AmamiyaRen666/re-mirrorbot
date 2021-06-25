d6802a0497fd84269e34aa5aeb6184d154a8ba48
Ncode2014 <32153003+Ncode2014@users.noreply.github.com>
2021-06-25 19:33:10 +0700

    some tweak for that
    i not sure about that but maybe usefull if some file has redirect but i think you can configurated as 1 if you have something like /download in your link but because i only have  direct file so its unnecessary but if you want you can chance that 
    
    according to this https://stackoverflow.com/a/2662955

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 if [[ -n $TOKEN_PICKLE_URL ]]; then
-	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
+	wget --max-redirect=0 -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
 fi
 
 if [[ -n $ACCOUNTS_ZIP_URL ]]; then
-	wget -q $ACCOUNTS_ZIP_URL -O /usr/src/app/accounts.zip
+	wget --max-redirect=0 -q $ACCOUNTS_ZIP_URL -O /usr/src/app/accounts.zip
 	unzip accounts.zip -d -qq /usr/src/app/accounts
 	rm accounts.zip
 fi
