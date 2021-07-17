breakdowns/slam-mirrorbot

Add optional status limit with buttons -Minor changes Note: I recommend limit status to 4 tasks max
Signed-off-by: anas <e.anastayyar@gmail.com>

bot/helper/ext_utils/bot_utils.py
@@ -2,15 +2,22 @@ import logging
 import re
 import threading
 import time
+import math
 
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot import download_dict, download_dict_lock, FINISHED_PROGRESS_STR, UNFINISHED_PROGRESS_STR
+from bot import dispatcher, download_dict, download_dict_lock, FINISHED_PROGRESS_STR, UNFINISHED_PROGRESS_STR, STATUS_LIMIT
+from telegram import InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler
+from bot.helper.telegram_helper import button_build, message_utils
 
 LOGGER = logging.getLogger(__name__)
 
 MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
 
 URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
+
+COUNT = 0
+PAGE_NO = 1
 
 
 class MirrorStatus:
@@ -99,29 +106,71 @@ def get_progress_bar_string(status):
 def get_readable_message():
     with download_dict_lock:
         msg = ""
+        INDEX = 0
+        if STATUS_LIMIT is not None:
+            dick_no = len(download_dict)
+            global pages
+            pages = math.ceil(dick_no/STATUS_LIMIT)
+            if PAGE_NO > pages and pages != 0:
+                globals()['COUNT'] -= STATUS_LIMIT
+                globals()['PAGE_NO'] -= 1
         for download in list(download_dict.values()):
-            msg += f"<b>Filename:</b> <code>{download.name()}</code>"
-            msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
-            if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
+            INDEX += 1
+            if INDEX > COUNT:
+                msg += f"<b>Filename:</b> <code>{download.name()}</code>"
+                msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
+                if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
+                    msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
+                    if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                        msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    else:
+                        msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n<b>Speed:</b> {download.speed()}" \
+                            f", <b>ETA:</b> {download.eta()} "
+                    # if hasattr(download, 'is_torrent'):
+                    try:
+                        msg += f"\n<b>Seeders:</b> {download.aria_download().num_seeders}" \
+                            f" | <b>Peers:</b> {download.aria_download().connections}"
+                    except:
+                        pass
+                    msg += f'\n<b>User:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a> (<code>{download.message.from_user.id}</code>)'
                 if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                    msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                else:
-                    msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>Speed:</b> {download.speed()}" \
-                        f", <b>ETA:</b> {download.eta()} "
-                # if hasattr(download, 'is_torrent'):
-                try:
-                    msg += f"\n<b>Seeders:</b> {download.aria_download().num_seeders}" \
-                        f" | <b>Peers:</b> {download.aria_download().connections}"
-                except:
-                    pass
-                msg += f'\n<b>User:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a> (<code>{download.message.from_user.id}</code>)'
-            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<b>To Stop:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
-            msg += "\n\n"
-        return msg
+                    msg += f"\n<b>To Stop:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += "\n\n"
+                if STATUS_LIMIT is not None:
+                    limit = COUNT + STATUS_LIMIT
+                    if INDEX >= limit:
+                        break
+        if STATUS_LIMIT is not None:
+            if INDEX > limit:
+                return None, None
+            msg += f"Page: {PAGE_NO}/{pages} | Tasks: {dick_no}\n"
+            buttons = button_build.ButtonMaker()
+            buttons.sbutton("Previous", "pre")
+            buttons.sbutton("Next", "nex")
+            button = InlineKeyboardMarkup(buttons.build_menu(2))
+            return msg, button
+        return msg, ""
 
+def flip(update, context):
+    query = update.callback_query
+    query.answer()
+    global COUNT, PAGE_NO
+    if query.data == "nex":
+        if PAGE_NO == pages:
+            COUNT = 0
+            PAGE_NO = 1
+        else:
+            COUNT += STATUS_LIMIT
+            PAGE_NO += 1
+    elif query.data == "pre":
+        if PAGE_NO == 1:
+            COUNT = STATUS_LIMIT * (pages - 1)
+            PAGE_NO = pages
+        else:
+            COUNT -= STATUS_LIMIT
+            PAGE_NO -= 1
+    message_utils.update_all_messages()
 
 def get_readable_time(seconds: int) -> str:
     result = ''
@@ -180,3 +229,8 @@ def new_thread(fn):
         return thread
 
     return wrapper
+
+next_handler = CallbackQueryHandler(flip, pattern="nex", run_async=True)
+previous_handler = CallbackQueryHandler(flip, pattern="pre", run_async=True)
+dispatcher.add_handler(next_handler)
+dispatcher.add_handler(previous_handler)

AmamiyaRen666/re-mirrorbot

bot/helper/ext_utils/bot_utils.py
@@ -2,15 +2,22 @@ import logging
 import re
 import threading
 import time
+import math
 
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot import download_dict, download_dict_lock, FINISHED_PROGRESS_STR, UNFINISHED_PROGRESS_STR
+from bot import dispatcher, download_dict, download_dict_lock, FINISHED_PROGRESS_STR, UNFINISHED_PROGRESS_STR, STATUS_LIMIT
+from telegram import InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler
+from bot.helper.telegram_helper import button_build, message_utils
 
 LOGGER = logging.getLogger(__name__)
 
 MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
 
 URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
+
+COUNT = 0
+PAGE_NO = 1
 
 
 class MirrorStatus:
@@ -99,29 +106,71 @@ def get_progress_bar_string(status):
 def get_readable_message():
     with download_dict_lock:
         msg = ""
+        INDEX = 0
+        if STATUS_LIMIT is not None:
+            dick_no = len(download_dict)
+            global pages
+            pages = math.ceil(dick_no/STATUS_LIMIT)
+            if PAGE_NO > pages and pages != 0:
+                globals()['COUNT'] -= STATUS_LIMIT
+                globals()['PAGE_NO'] -= 1
         for download in list(download_dict.values()):
-            msg += f"<b>Namafile:</b> <code>{download.name()}</code>"
-            msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
-            if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
+            INDEX += 1
+            if INDEX > COUNT:
+                msg += f"<b>Namafile:</b> <code>{download.name()}</code>"
+                msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
+                if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
+                    msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
+                    if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                        msg += f"\n<b>Diunduh:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    else:
+                        msg += f"\n<b>Diunggah:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n<b>Kecepatan:</b> {download.speed()}" \
+                            f", <b>Kapan:</b> {download.eta()} "
+                    # if hasattr(download, 'is_torrent'):
+                    try:
+                        msg += f"\n<b>Seeders:</b> {download.aria_download().num_seeders}" \
+                            f" | <b>Peers:</b> {download.aria_download().connections}"
+                    except:
+                        pass
+                    msg += f'\n<b>Pengguna:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a>'
                 if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                    msg += f"\n<b>Diunduh:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                else:
-                    msg += f"\n<b>Diunggah:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>Kecepatan:</b> {download.speed()}" \
-                        f", <b>Kapan:</b> {download.eta()} "
-                # if hasattr(download, 'is_torrent'):
-                try:
-                    msg += f"\n<b>Seeders:</b> {download.aria_download().num_seeders}" \
-                        f" | <b>Peers:</b> {download.aria_download().connections}"
-                except:
-                    pass
-                msg += f'\n<b>Pengguna:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a> (<code>{download.message.from_user.id}</code>)'
-            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<b>Untuk berhenti:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
-            msg += "\n\n"
-        return msg
+                    msg += f"\n<b>Untuk berhenti:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += "\n\n"
+                if STATUS_LIMIT is not None:
+                    limit = COUNT + STATUS_LIMIT
+                    if INDEX >= limit:
+                        break
+        if STATUS_LIMIT is not None:
+            if INDEX > limit:
+                return None, None
+            msg += f"Halaman: {PAGE_NO}/{pages} | Tugas: {dick_no}\n"
+            buttons = button_build.ButtonMaker()
+            buttons.sbutton("Sebelumnya", "pre")
+            buttons.sbutton("Selanjutnya", "nex")
+            button = InlineKeyboardMarkup(buttons.build_menu(2))
+            return msg, button
+        return msg, ""
 
+def flip(update, context):
+    query = update.callback_query
+    query.answer()
+    global COUNT, PAGE_NO
+    if query.data == "nex":
+        if PAGE_NO == pages:
+            COUNT = 0
+            PAGE_NO = 1
+        else:
+            COUNT += STATUS_LIMIT
+            PAGE_NO += 1
+    elif query.data == "pre":
+        if PAGE_NO == 1:
+            COUNT = STATUS_LIMIT * (pages - 1)
+            PAGE_NO = pages
+        else:
+            COUNT -= STATUS_LIMIT
+            PAGE_NO -= 1
+    message_utils.update_all_messages()
 
 def get_readable_time(seconds: int) -> str:
     result = ''
@@ -180,3 +229,8 @@ def new_thread(fn):
         return thread
 
     return wrapper
+
+next_handler = CallbackQueryHandler(flip, pattern="nex", run_async=True)
+previous_handler = CallbackQueryHandler(flip, pattern="pre", run_async=True)
+dispatcher.add_handler(next_handler)
+dispatcher.add_handler(previous_handler)