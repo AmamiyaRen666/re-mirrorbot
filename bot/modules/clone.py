b4dc0b232d398a448d91457d736793f4b11fe509
Anas <77075674+anasty17@users.noreply.github.com>
2021-07-19 14:43:04 +0300

    Missed exception

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -77,7 +77,7 @@ def cloneNode(update, context):
                 delete_all_messages()
             else:
                 update_all_messages()
-        except IndexError:
+        except IndexError, KeyError:
             pass
     else:
         sendMessage(
