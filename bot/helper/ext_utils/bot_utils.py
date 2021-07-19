breakdowns/slam-mirrorbot

Add Clone Status -add clone status for folders have more than 15 files -clean up
Signed-off-by: anas <e.anastayyar@gmail.com>

bot/helper/ext_utils/bot_utils.py
@@ -23,6 +23,7 @@ PAGE_NO = 1
 class MirrorStatus:
     STATUS_UPLOADING = "Uploading...📤"
     STATUS_DOWNLOADING = "Downloading...📥"
+    STATUS_CLONING = "Cloning...♻️"
     STATUS_WAITING = "Queued...📝"
     STATUS_FAILED = "Failed 🚫. Cleaning Download..."
     STATUS_ARCHIVING = "Archiving...🔐"
@@ -123,6 +124,8 @@ def get_readable_message():
                     msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
                     if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                         msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    elif download.status() == MirrorStatus.STATUS_CLONING:
+                        msg += f"\n<b>Cloned:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     else:
                         msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     msg += f"\n<b>Speed:</b> {download.speed()}" \
@@ -134,7 +137,7 @@ def get_readable_message():
                     except:
                         pass
                     msg += f'\n<b>User:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a> (<code>{download.message.from_user.id}</code>)'
-                if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                if download.status() == MirrorStatus.STATUS_DOWNLOADING or download.status() == MirrorStatus.STATUS_CLONING:
                     msg += f"\n<b>To Stop:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
                 msg += "\n\n"
                 if STATUS_LIMIT is not None:

bot/helper/telegram_helper/message_utils.py
@@ -93,7 +93,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n<b>FREE:</b> {free} | <b>UPTIME:</b> {currentTime}\n<b>DL:</b> {dlspeed}ps 🔻 | <b>UL:</b> {ulspeed}ps 🔺\n"
+        msg += f"\n<b>FREE:</b> {free} | <b>UPTIME:</b> {currentTime}\n<b>DL:</b> {dlspeed}/s 🔻 | <b>UL:</b> {ulspeed}/s 🔺\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -134,7 +134,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>FREE:</b> {free} | <b>UPTIME:</b> {currentTime}\n<b>DL:</b> {dlspeed}ps 🔻 | <b>UL:</b> {ulspeed}ps 🔺\n"
+        progress += f"\n<b>FREE:</b> {free} | <b>UPTIME:</b> {currentTime}\n<b>DL:</b> {dlspeed}/s 🔻 | <b>UL:</b> {ulspeed}/s 🔺\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:

bot/modules/clone.py
@@ -1,56 +1,84 @@
 from telegram.ext import CommandHandler
-from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.message_utils import sendMarkup, deleteMessage, sendMessage
+from bot.helper.mirror_utils.upload_utils import gdriveTools
+from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE_CLONE
-from bot.helper.ext_utils.bot_utils import get_readable_file_size
+from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
+from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE_CLONE, download_dict, download_dict_lock, Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, setInterval
+import random
+import string
 
 
 def cloneNode(update, context):
     args = update.message.text.split(" ", maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        gd = GoogleDriveHelper()
-        if CLONE_LIMIT is not None or STOP_DUPLICATE_CLONE:
-            res, clonesize, name = gd.clonehelper(link)
-            if res != "":
-               sendMessage(res, context.bot, update)
-               return
-            if STOP_DUPLICATE_CLONE:
-                LOGGER.info(f"Checking File/Folder if already in Drive...")
-                smsg, button = gd.drive_list(name)
-                if smsg:
-                    msg3 = "File/Folder is already available in Drive.\nHere are the search results:"
-                    sendMarkup(msg3, context.bot, update, button)
+        gd = gdriveTools.GoogleDriveHelper()
+        res, clonesize, name, files = gd.clonehelper(link)
+        if res != "":
+            sendMessage(res, context.bot, update)
+            return
+        if STOP_DUPLICATE_CLONE:
+            LOGGER.info(f"Checking File/Folder if already in Drive...")
+            smsg, button = gd.drive_list(name)
+            if smsg:
+                msg3 = "File/Folder is already available in Drive.\nHere are the search results:"
+                sendMarkup(msg3, context.bot, update, button)
+                return
+        if CLONE_LIMIT is not None:
+            LOGGER.info(f"Checking File/Folder Size...")
+            limit = CLONE_LIMIT
+            limit = limit.split(' ', maxsplit=1)
+            limitint = int(limit[0])
+            msg2 = f'Failed, Clone limit is {CLONE_LIMIT}.\nYour File/Folder size is {get_readable_file_size(clonesize)}.'
+            if 'G' in limit[1] or 'g' in limit[1]:
+                if clonesize > limitint * 1024**3:
+                    sendMessage(msg2, context.bot, update)
                     return
-            if CLONE_LIMIT is not None:
-                LOGGER.info(f"Checking File/Folder Size...")
-                limit = CLONE_LIMIT
-                limit = limit.split(' ', maxsplit=1)
-                limitint = int(limit[0])
-                msg2 = f'Failed, Clone limit is {CLONE_LIMIT}.\nYour File/Folder size is {get_readable_file_size(clonesize)}.'
-                if 'G' in limit[1] or 'g' in limit[1]:
-                    if clonesize > limitint * 1024**3:
-                        sendMessage(msg2, context.bot, update)
-                        return
-                elif 'T' in limit[1] or 't' in limit[1]:
-                    if clonesize > limitint * 1024**4:
-                        sendMessage(msg2, context.bot, update)
-                        return              
-        msg = sendMessage(f"Cloning: <code>{link}</code>", context.bot, update)
-        result, button = gd.clone(link)
-        deleteMessage(context.bot, msg)
-        if button == "":
-            sendMessage(result, context.bot, update)
+            elif 'T' in limit[1] or 't' in limit[1]:
+                if clonesize > limitint * 1024**4:
+                    sendMessage(msg2, context.bot, update)
+                    return              
+        if files < 15:
+            msg = sendMessage(f"Cloning: <code>{link}</code>", context.bot, update)
+            result, button = gd.clone(link)
+            deleteMessage(context.bot, msg)
         else:
-            if update.message.from_user.username:
-                uname = f'@{update.message.from_user.username}'
-            else:
-                uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
-            if uname is not None:
-                cc = f'\n\ncc: {uname}'
+            drive = gdriveTools.GoogleDriveHelper(name)
+            gid = ''.join(random.SystemRandom().choices(string.ascii_letters + string.digits, k=12))
+            clone_status = CloneStatus(drive, clonesize, update, gid)
+            with download_dict_lock:
+                download_dict[update.message.message_id] = clone_status
+            if len(Interval) == 0:
+                Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
+            sendStatusMessage(update, context.bot)
+            result, button = drive.clone(link)
+        if update.message.from_user.username:
+            uname = f'@{update.message.from_user.username}'
+        else:
+            uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+        if uname is not None:
+            cc = f'\n\ncc: {uname}'
+            men = f'{uname} '
+        if button == "cancelled":
+            sendMessage(men + result, context.bot, update)
+        elif button == "":
+            sendMessage(men + result, context.bot, update)
+        else:
             sendMarkup(result + cc, context.bot, update, button)
+        try:
+            with download_dict_lock:
+                del download_dict[update.message.message_id]
+                count = len(download_dict)
+            if count == 0:
+                Interval[0].cancel()
+                del Interval[0]
+                delete_all_messages()
+            else:
+                update_all_messages()
+        except IndexError:
+            pass
     else:
         sendMessage('Provide G-Drive Shareable Link to Clone.', context.bot, update)
 

AmamiyaRen666/re-mirrorbot

bot/helper/ext_utils/bot_utils.py
@@ -23,6 +23,7 @@ PAGE_NO = 1
 class MirrorStatus:
     STATUS_UPLOADING = "Uploading...📤"
     STATUS_DOWNLOADING = "Downloading...📥"
+    STATUS_CLONING = "Cloning...♻️"
     STATUS_WAITING = "Queued...📝"
     STATUS_FAILED = "Failed 🚫. Cleaning Download..."
     STATUS_ARCHIVING = "Archiving...🔐"
@@ -127,6 +128,8 @@ def get_readable_message():
                     msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
                     if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                         msg += f"\n<b>Diunduh:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    elif download.status() == MirrorStatus.STATUS_CLONING:
+                        msg += f"\n<b>Kloning:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     else:
                         msg += f"\n<b>Diunggah:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     msg += f"\n<b>Kecepatan:</b> {download.speed()}" \
@@ -138,7 +141,7 @@ def get_readable_message():
                     except:
                         pass
                     msg += f'\n<b>Pengguna:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a>'
-                if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                if download.status() == MirrorStatus.STATUS_DOWNLOADING or download.status() == MirrorStatus.STATUS_CLONING:
                     msg += f"\n<b>Untuk berhenti:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
                 msg += "\n\n"
                 if STATUS_LIMIT is not None:

bot/helper/telegram_helper/message_utils.py
@@ -115,7 +115,7 @@ def update_all_messages():
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n<b>Bebas:</b> {free} | <b>Berjalan:</b> {currentTime}\n<b>DL:</b> {dlspeed}ps 🔻 | <b>UL:</b> {ulspeed}ps 🔺\n"
+        msg += f"\n<b>Bebas:</b> {free} | <b>Berjalan:</b> {currentTime}\n<b>DL:</b> {dlspeed}/s 🔻 | <b>UL:</b> {ulspeed}/s 🔺\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id
@@ -157,7 +157,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>FREE:</b> {free} | <b>UPTIME:</b> {currentTime}\n<b>DL:</b> {dlspeed}ps 🔻 | <b>UL:</b> {ulspeed}ps 🔺\n"
+        progress += f"\n<b>FREE:</b> {free} | <b>UPTIME:</b> {currentTime}\n<b>DL:</b> {dlspeed}/s 🔻 | <b>UL:</b> {ulspeed}/s 🔺\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:

bot/modules/clone.py
@@ -1,56 +1,84 @@
 from telegram.ext import CommandHandler
-from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.message_utils import sendMarkup, deleteMessage, sendMessage
+from bot.helper.mirror_utils.upload_utils import gdriveTools
+from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE_CLONE
-from bot.helper.ext_utils.bot_utils import get_readable_file_size
+from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
+from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE_CLONE, download_dict, download_dict_lock, Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, setInterval
+import random
+import string
 
 
 def cloneNode(update, context):
     args = update.message.text.split(" ", maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        gd = GoogleDriveHelper()
-        if CLONE_LIMIT is not None or STOP_DUPLICATE_CLONE:
-            res, clonesize, name = gd.clonehelper(link)
-            if res != "":
-                sendMessage(res, context.bot, update)
+        gd = gdriveTools.GoogleDriveHelper()
+        res, clonesize, name, files = gd.clonehelper(link)
+        if res != "":
+            sendMessage(res, context.bot, update)
+            return
+        if STOP_DUPLICATE_CLONE:
+            LOGGER.info(f"Memeriksa File/Folder jika sudah di Drive...")
+            smsg, button = gd.drive_list(name)
+            if smsg:
+                msg3 = "File/Folder sudah tersedia di Drive.\nBerikut adalah hasil pencariannya:"
+                sendMarkup(msg3, context.bot, update, button)
                 return
-            if STOP_DUPLICATE_CLONE:
-                LOGGER.info(f"Memeriksa File/Folder jika sudah di Drive...")
-                smsg, button = gd.drive_list(name)
-                if smsg:
-                    msg3 = "File/folder sudah tersedia di drive.\nBerikut adalah hasil pencarian:"
-                    sendMarkup(msg3, context.bot, update, button)
+        if CLONE_LIMIT is not None:
+            LOGGER.info(f"Memeriksa Ukuran File/Folder...")
+            limit = CLONE_LIMIT
+            limit = limit.split(' ', maxsplit=1)
+            limitint = int(limit[0])
+            msg2 = f'Gagal, Dikarenakan anda melewati batas Klon yang diatur sebesar {CLONE_LIMIT}.\nUkuran File/Folder Anda adalah {get_readable_file_size(clonesize)}.'
+            if 'G' in limit[1] or 'g' in limit[1]:
+                if clonesize > limitint * 1024**3:
+                    sendMessage(msg2, context.bot, update)
                     return
-            if CLONE_LIMIT is not None:
-                LOGGER.info(f"Memeriksa ukuran file/folder...")
-                limit = CLONE_LIMIT
-                limit = limit.split(' ', maxsplit=1)
-                limitint = int(limit[0])
-                msg2 = f'Gagal, batas klon adalah {CLONE_LIMIT}.\nUkuran file / folder Anda {get_readable_file_size(clonesize)}.'
-                if 'G' in limit[1] or 'g' in limit[1]:
-                    if clonesize > limitint * 1024**3:
-                        sendMessage(msg2, context.bot, update)
-                        return
-                elif 'T' in limit[1] or 't' in limit[1]:
-                    if clonesize > limitint * 1024**4:
-                        sendMessage(msg2, context.bot, update)
-                        return
-        msg = sendMessage(f"Kloning: <code>{link}</code>", context.bot, update)
-        result, button = gd.clone(link)
-        deleteMessage(context.bot, msg)
-        if button == "":
-            sendMessage(result, context.bot, update)
+            elif 'T' in limit[1] or 't' in limit[1]:
+                if clonesize > limitint * 1024**4:
+                    sendMessage(msg2, context.bot, update)
+                    return              
+        if files < 15:
+            msg = sendMessage(f"Diklon: <code>{link}</code>", context.bot, update)
+            result, button = gd.clone(link)
+            deleteMessage(context.bot, msg)
         else:
-            if update.message.from_user.username:
-                uname = f'@{update.message.from_user.username}'
-            else:
-                uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
-            if uname is not None:
-                cc = f'\n\nDari: {uname}'
+            drive = gdriveTools.GoogleDriveHelper(name)
+            gid = ''.join(random.SystemRandom().choices(string.ascii_letters + string.digits, k=12))
+            clone_status = CloneStatus(drive, clonesize, update, gid)
+            with download_dict_lock:
+                download_dict[update.message.message_id] = clone_status
+            if len(Interval) == 0:
+                Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
+            sendStatusMessage(update, context.bot)
+            result, button = drive.clone(link)
+        if update.message.from_user.username:
+            uname = f'@{update.message.from_user.username}'
+        else:
+            uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+        if uname is not None:
+            cc = f'\n\nDari: {uname}'
+            men = f'{uname} '
+        if button == "cancelled":
+            sendMessage(men + result, context.bot, update)
+        elif button == "":
+            sendMessage(men + result, context.bot, update)
+        else:
             sendMarkup(result + cc, context.bot, update, button)
+        try:
+            with download_dict_lock:
+                del download_dict[update.message.message_id]
+                count = len(download_dict)
+            if count == 0:
+                Interval[0].cancel()
+                del Interval[0]
+                delete_all_messages()
+            else:
+                update_all_messages()
+        except IndexError:
+            pass
     else:
         sendMessage(
             'Provide G-Drive Shareable Link to Clone.', context.bot, update